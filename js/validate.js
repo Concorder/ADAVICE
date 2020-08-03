54a528e9f964904c9c9643054267a96f36f57b3b
Concorder <56825826+Concorder@users.noreply.github.com>
2020-08-03 10:23:21 +0300

    556

--- js/validate.js
+++ js/validate.js
@@ -1,14 +1,6 @@
-$('form').on('submit',function(){
-	document.getElementById('popup_content').style.display = 'block';
-});
 
-let closePop = document.getElementById('closePop');
-closePop.addEventListener('click', hidePopUp);
 
-function hidePopUp() {
-	bluePop.style.display = 'none';
-}
 
-let bluePop = document.getElementsByClassName('thank-you');
+
 
 
